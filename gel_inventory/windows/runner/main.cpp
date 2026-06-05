d9115c8596f19f373968669f955c123ee5409c9a
jlumapas <lumapasj.23@gmail.com>
2026-06-05 11:50:06 +0800

    adjust window size and position

--- gel_inventory/windows/runner/main.cpp
+++ gel_inventory/windows/runner/main.cpp
@@ -25,8 +25,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
+  Win32Window::Size size(1280, 860);
+  const int screenW = ::GetSystemMetrics(SM_CXSCREEN);
+  const int screenH = ::GetSystemMetrics(SM_CYSCREEN);
+  Win32Window::Point origin((screenW - size.width) / 2,
+                            (screenH - size.height) / 2);
   if (!window.Create(L"GEL's Inventory", origin, size)) {
     return EXIT_FAILURE;
   }
